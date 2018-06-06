2a50e7d07e3b443ff8755fd499bcdc57d9a98ff2
Dmitry Vilesov <vilesov@gmail.com>
2018-06-06 23:57:04 +0000

    websockets server

--- server.py
+++ server.py
@@ -4,6 +4,13 @@ from bleserver import GattRPCServer
 from scan import *
 from weight_scale import *
 import time
+
+import asyncio
+import websockets
+#from jsonrpcserver.aio import methods
+#from jsonrpcserver.response import NotificationResponse
+
+
 DEVICE = 'hci0'
 
 @methods.add
@@ -19,8 +26,20 @@ def disconnect(context):
     while context.requester.is_connected():
         time.sleep(1)    
     return True
-    
+
+@asyncio.coroutine    
+def main(websocket, path):
+    request = yield from websocket.recv()
+    response = methods.dispatch(request,context=bleserver)
+    if not response.is_notification:
+        yield from websocket.send(str(response))
+
 class TestHttpServer(BaseHTTPRequestHandler):
+    def do_OPTIONS(self):           
+        self.send_response(200, "ok")       
+        self.send_header('Access-Control-Allow-Origin', '*')                
+        self.send_header('Access-Control-Allow-Methods', 'POST, OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "X-Requested-With")
     def do_POST(self):
         # Process request
         request = self.rfile.read(int(self.headers['Content-Length'])).decode()
@@ -34,4 +53,10 @@ class TestHttpServer(BaseHTTPRequestHandler):
 
 if __name__ == '__main__':
     bleserver = GattRPCServer(DEVICE)
-    HTTPServer(('localhost', 5001), TestHttpServer).serve_forever()
+    #HTTPServer(('localhost', 5001), TestHttpServer).serve_forever()
+    
+    start_server = websockets.serve(main, 'localhost', 5000)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+    #asyncio.get_event_loop().run_until_complete(start_server)
+    #asyncio.get_event_loop().run_forever()
